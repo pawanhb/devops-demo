d19b93b27c4926e3a6cd924ed1cbf5922c3f8ebc
pawanhb <pawanhb21@gmail.com>
2025-03-27 16:26:01 +0530

    POM changes

--- src/main/java/com/devops/demo/controllers/DevopsDemoRestController.java
+++ src/main/java/com/devops/demo/controllers/DevopsDemoRestController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DevopsDemoRestController {
 
-    @GetMapping("/hello")
-    public String helloMessage(){
+    @GetMapping("/hello1")
+    public String helloMessage1(){
+        return "hello";
+    }
+
+    @GetMapping("/hello12")
+    public String helloMessage12(){
+        return "hello";
+    }
+
+    @GetMapping("/hello123")
+    public String helloMessage123(){
+        return "hello";
+    }
+
+    @GetMapping("/hello1234")
+    public String helloMessage1234(){
         return "hello";
     }
 }
